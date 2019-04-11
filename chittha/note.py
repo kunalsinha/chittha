93fcaf532d7fa5e31cc3cf9510edb62b5028a897
kunal sinha <kunalsinha4u@gmail.com>
2019-04-12 03:35:27 +0530

    added widgets to note

--- chittha/note.py
+++ chittha/note.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTextEdit, QStatusBar
 from PyQt5.QtCore import Qt, QPoint
 
 import logging
@@ -17,12 +17,21 @@ class Note(QWidget):
         self.setMinimumWidth(self.MIN_WIDTH)
         self.setMinimumHeight(self.MIN_HEIGHT)
         self.resize(self.INITIAL_WIDTH, self.INITIAL_HEIGHT)
+        # make the note widgets skip taskbar
         self.setWindowFlags(Qt.Tool)
         self.currentPosition = None
+        # add a topmost vertical layout to the note
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(NoteMenu(self))
+        self.layout.addWidget(NoteEditor(self))
+        self.layout.addWidget(NoteStatus(self))
+        self.setLayout(self.layout)
 
     def hideNote(self):
-        self.currentPosition = self.pos()
+        if not self.isHidden:
+            self.currentPosition = self.pos()
         self.hide()
+        self.isHidden = True
         logger.error('Hiding at: ' + str(self.currentPosition))
 
     def showNote(self):
@@ -32,3 +41,20 @@ class Note(QWidget):
             logger.error('Moving to: ' + str(self.currentPosition))
         self.show()
         self.activateWindow()
+        self.isHidden = False
+
+class NoteMenu(QWidget):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+class NoteEditor(QTextEdit):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+class NoteStatus(QStatusBar):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
