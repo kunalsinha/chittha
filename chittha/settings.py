ba052c568d2115e1ea2fa69caf391418dc17a7ab
kunal sinha <kunalsinha4u@gmail.com>
2019-04-23 17:02:19 +0530

    license string update

--- chittha/settings.py
+++ chittha/settings.py
@@ -1,10 +1,18 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'settings.ui'
+# Chittha - a sticky notes application
+# Copyright (C) 2019 Kunal Sinha <kunalsinha4u@gmail.com>
 #
-# Created by: PyQt5 UI code generator 5.12.1
+# Chittha is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# WARNING! All changes made in this file will be lost!
+# Chittha is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Chittha.  If not, see <https://www.gnu.org/licenses/>.
 
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QFont
