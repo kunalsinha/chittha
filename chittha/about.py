a61e5350454594f4940733a1b9cda2e28581e4dd
kunal sinha <kunalsinha4u@gmail.com>
2019-04-24 21:30:05 +0530

    added shortcut info in about page

--- chittha/about.py
+++ chittha/about.py
@@ -1,10 +1,18 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'about.ui'
+# Chittha - a sticky notes application
+# Copyright (C) 2019 Kunal Sinha <kunalsinha4u@gmail.com>
 #
-# Created by: PyQt5 UI code generator 5.12.1
+# Chittha is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# WARNING! All changes made in this file will be lost!
+# Chittha is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Chittha.  If not, see <https://www.gnu.org/licenses/>.
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
@@ -12,12 +20,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_TabWidget(object):
     def setupUi(self, TabWidget):
         TabWidget.setObjectName("TabWidget")
-        TabWidget.resize(540, 331)
+        TabWidget.resize(540, 351)
         self.about = QtWidgets.QWidget()
         self.about.setEnabled(True)
         self.about.setObjectName("about")
         self.label_2 = QtWidgets.QLabel(self.about)
-        self.label_2.setGeometry(QtCore.QRect(10, 0, 511, 291))
+        self.label_2.setGeometry(QtCore.QRect(10, 0, 511, 321))
         self.label_2.setObjectName("label_2")
         TabWidget.addTab(self.about, "")
         self.license = QtWidgets.QWidget()
@@ -34,7 +42,7 @@ class Ui_TabWidget(object):
     def retranslateUi(self, TabWidget):
         _translate = QtCore.QCoreApplication.translate
         TabWidget.setWindowTitle(_translate("TabWidget", "Chittha"))
-        self.label_2.setText(_translate("TabWidget", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; font-weight:600;\">Chittha v1.0</span></p><p align=\"center\"><span style=\" font-weight:600;\">Keyboard Shortcuts</span></p><p align=\"center\">Ctrl + N -&gt; New note</p><p align=\"center\">Ctrl + J -&gt; Next note</p><p align=\"center\">Ctrl + K -&gt; Previous note</p><p align=\"center\">Ctrl + H -&gt; Hide all notes</p><p align=\"center\"><br/></p><p align=\"center\"><a href=\"https://github.com/kunalsinha/chittha\"><span style=\" text-decoration: underline; color:#0000ff;\">https://github.com/kunalsinha/chittha</span></a></p><p align=\"center\">Copyright (C) 2019 Kunal Sinha</p></body></html>"))
+        self.label_2.setText(_translate("TabWidget", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; font-weight:600;\">Chittha v1.0</span></p><p align=\"center\"><span style=\" font-weight:600;\">Keyboard Shortcuts</span></p><p align=\"center\">Ctrl + N -&gt; New note</p><p align=\"center\">Ctrl + J -&gt; Next note</p><p align=\"center\">Ctrl + K -&gt; Previous note</p><p align=\"center\">Ctrl + L -&gt; Lock/Unlock note</p><p align=\"center\">Ctrl + H -&gt; Hide all notes</p><p align=\"center\"><br/></p><p align=\"center\"><a href=\"https://github.com/kunalsinha/chittha\"><span style=\" text-decoration: underline; color:#0000ff;\">https://github.com/kunalsinha/chittha</span></a></p><p align=\"center\">Copyright (C) 2019 Kunal Sinha</p></body></html>"))
         TabWidget.setTabText(TabWidget.indexOf(self.about), _translate("TabWidget", "About"))
         self.label.setText(_translate("TabWidget", "Chittha is free software: you can redistribute it and/or modify\n"
 "it under the terms of the GNU General Public License as published by\n"
